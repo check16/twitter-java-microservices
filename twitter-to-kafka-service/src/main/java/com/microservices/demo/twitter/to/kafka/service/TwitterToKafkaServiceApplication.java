35dfa9172aac5e6e5ec0c94058ccd4c70f08364b
Antonio Sanchez <antonio.san.87@hotmail.com>
2021-07-02 19:17:59 +0200

    Create Twitter stream for keywords.

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,11 +1,27 @@
 package com.microservices.demo.twitter.to.kafka.service;
 
+import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+
+    private final TwitterToKafkaServiceConfigData configData;
+    private final StreamRunner streamRunner;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData, StreamRunner runner) {
+        this.configData = configData;
+        this.streamRunner = runner;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
@@ -13,6 +29,9 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Application starts...");
+        LOG.info("Application starts...");
+        LOG.info(Arrays.toString(configData.getTwitterKeywords().toArray(new String[]{})));
+        LOG.info(configData.getWelcomeMessage());
+        streamRunner.start();
     }
 }
